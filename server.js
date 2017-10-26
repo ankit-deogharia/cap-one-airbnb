80f6fc93fe145184af238c03ccda973e2f911b41
Ankit Deogharia <ankit.deogharia@gmail.com>
2017-10-26 00:57:06 -0400

    Serving angular application

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 var express = require("express");
+var bodyParser = require("body-parser")
 var app = express();
+
+app.use(bodyParser.json());
+
+//Link to Angular build directory
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
 
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
@@ -12,6 +19,31 @@ var server = app.listen(process.env.PORT || 8080, function () {
     console.log("App now running on port", port);
 });
 
+//Base API endpoint
 app.get("/", function(req, res) {
 	res.send("Hello World");
+});
+
+//API endpoint for estimating weekly average income given lat and long
+app.post("/average", function(req, res) {
+	var lat = req.body.lat;
+	var long = req.body.long;
+
+	if (!lat || !long) {
+		handleError(res, "Invalid user input", "User must provide both latitude and longitude", 400);
+	}
+
+	//Data processing logic goes here
+});
+
+//API endpoint for estimating ideal price per night given lat and long
+app.post("/average", function(req, res) {
+	var lat = req.body.lat;
+	var long = req.body.long;
+
+	if (!lat || !long) {
+		handleError(res, "Invalid user input", "User must provide both latitude and longitude", 400);
+	}
+
+	//Data processing logic goes here
 });
